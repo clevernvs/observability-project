e3f8e0a2030ef6e45f4885208d3e64d2d45a0a04
Cleverton Neves <clever.nvs@gmail.com>
2025-10-06 19:06:52 -0300

    feat: Iniciando o projeto

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import client from "prom-client";
+
+const app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+const counter = new client.Counter({
+    name: "http_requests_total",
+    help: "Número total de requisições HTTP recebidas",
+});
+
+app.get("/", (req, res) => {
+    counter.inc();
+    res.send("Olá, Cleverton! 🚀");
+});
+
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", client.register.contentType);
+    res.end(await client.register.metrics());
+});
+
+app.listen(3000, () => console.log("🚀 Servidor rodando em http://localhost:3000"));
